5c248ccf25d1a2597c8439e1c361ad85513e57ef
J. Gregg Thomason <gregg.thomason@asti-usa.com>
2012-06-12 09:22:54 -0400

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='CouchTools',
-    version='0.4',
+    version='0.5',
     packages=['couchtools', ],
     license='Apache 2.0',
     long_description=open('NOTICE').read(),
